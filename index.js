dfab61146334cbc31eaa92361ee0accb5079d051
BangJack10111 <Amorntape2548l@gmail.com>
2025-08-08 14:38:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/', (req, res) => {
   res.send('hello world, อมรเทพ');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3012;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
